610b35caebcfa6021ab71f111f5ad29b49eb5d92
abhijitboral <abhijit1512.boral@gmail.com>
2025-07-25 16:48:18 +0530

    implemented rate limit on the production mode

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,30 @@ import os from "os";
 import dotenv from "dotenv";
 import path from "path";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 import UserRoutes from "./routes/userRoutes";
 import AuthenticationRoutes from "./routes/authenticationRoutes";
 import ConnectDb from "./db/connect";
 
+dotenv.config();
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') { 
+	app.use(
+		rateLimit({
+			windowMs: 15 * 60 * 1000, // 15 minutes
+			max: 100,
+			message: "Too many requests, please try again later.",
+		})
+	);
+}
 
 if (cluster.isPrimary) {
 	const numCPUs = os.cpus().length;
-	console.log(`CPU: ${numCPUs} , Master ${process.pid} is running`);
+	console.log(`CPU: ${numCPUs} , Master ${process.pid} is running on - ${process.env.NODE_ENV} environment`);
 	for (let i = 0; i < numCPUs; i++) {
 		cluster.fork();
 	}
@@ -20,13 +35,13 @@ if (cluster.isPrimary) {
 		console.log(`Worker ${worker.process.pid} died`);
 	});
 } else {
-	dotenv.config();
-	const app: Express = express();
+	
+	
 	const port: number = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 	const userRoutes: UserRoutes = new UserRoutes();
 	const authRoutes: AuthenticationRoutes = new AuthenticationRoutes();
-	app.use(express.json());
-	app.use(cors());
+	
+	
 	app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 	app.use("/api/auth", authRoutes.initRoutes());
 	app.use("/api/users", userRoutes.initRoutes());
